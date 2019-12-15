91d44c3203091ecbecb3d904b8ae052bb2fa4cf2
Kelvin Gaia Maués <kelvin.gaia@matchboxbrasil.com>
2019-12-15 13:08:40 -0300

    New asserts for sorted list

--- scratch/chapter_1/introduction.py
+++ scratch/chapter_1/introduction.py
@@ -1,5 +1,8 @@
 from __future__ import division
 
+
+# GOAL => Verify users relations
+
 # A small list of users - "dict"
 users = [
     { "id": 0, "name": "Hero" },
@@ -61,3 +64,29 @@ print("Avg connections", avg_connections)
 # Checking confidence
 assert num_users == 10
 assert avg_connections == 2.4
+
+# Also, find the people more connected
+# Sort from largest to smallest
+
+# Create a list (user_id, number_of_friends)
+number_of_friends_by_id = [(user["id"], number_of_friends(user)) for user in users]
+# Printing out the number of friends
+# Each pair is (user_id, num_friends):
+print("Number of friends", number_of_friends_by_id)
+
+# Sort friends
+number_of_friends_by_id.sort(key=lambda id_and_friends: id_and_friends[1], reverse=True)
+print("Number of friends sorted", number_of_friends_by_id)
+
+# Checking confidence
+assert number_of_friends_by_id[0][1] == 3 # several people have 3 friends
+assert number_of_friends_by_id[-1] == (9, 1) # user 9 has only 1 friend
+
+# These bunch of code above were important to compute
+# a metric network of degree of centrality
+# PT-BR: uma rede métrica de grau de centralidade
+# e pode ser representada por uma rede grafos determinando
+# suas conexões
+
+# GOAL => Estimulate more connection between the members
+# So, develop suggestion of "data scientists you may know"
