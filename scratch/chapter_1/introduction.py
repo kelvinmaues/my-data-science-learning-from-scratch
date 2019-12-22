b0c8afccda01d823955d2236351dae8c9c22e854
Kelvin Gaia Maués <kelvin.gaia@matchboxbrasil.com>
2019-12-22 12:01:18 -0300

    Fuction for most common interests with

--- scratch/chapter_1/introduction.py
+++ scratch/chapter_1/introduction.py
@@ -131,3 +131,65 @@ def friends_of_friends(user):
 print(friends_of_friends(users[3])) # Counter({0: 2, 5: 1})
 
 assert friends_of_friends(users[3]) == Counter({0: 2, 5: 1})
+
+# As a Data Scientist, you know you like to find users with
+# similar interests "competencia significativa"
+
+print(foaf_ids_bad(users[4]))
+
+# A list of pairs (user_id, interest)
+interests = [
+    (0, "Hadoop"), (0, "Big Data"), (0, "HBase"), (0, "Java"),
+    (0, "Spark"), (0, "Storm"), (0, "Cassandra"),
+    (1, "NoSQL"), (1, "MongoDB"), (1, "Cassandra"), (1, "HBase"),
+    (1, "Postgres"), (2, "Python"), (2, "scikit-learn"), (2, "scipy"),
+    (2, "numpy"), (2, "statsmodels"), (2, "pandas"), (3, "R"), (3, "Python"),
+    (3, "statistics"), (3, "regression"), (3, "probability"),
+    (4, "machine learning"), (4, "regression"), (4, "decision trees"),
+    (4, "libsvm"), (5, "Python"), (5, "R"), (5, "Java"), (5, "C++"),
+    (5, "Haskell"), (5, "programming languages"), (6, "statistics"),
+    (6, "probability"), (6, "mathematics"), (6, "theory"),
+    (7, "machine learning"), (7, "scikit-learn"), (7, "Mahout"),
+    (7, "neural networks"), (8, "neural networks"), (8, "deep learning"),
+    (8, "Big Data"), (8, "artificial intelligence"), (9, "Hadoop"),
+    (9, "Java"), (9, "MapReduce"), (9, "Big Data")
+]
+
+# Goal => Develop a function to find users with the same interests
+
+def data_scientist_who_like(target_interest):
+  """Find the ids of all users who like the target interest."""
+  return [user_id
+          for user_id, user_interest in interests
+          if user_interest == target_interest
+        ]
+
+# the function above works, but the entire list must be examined for
+# each serch. Could be better build an index of interests for users
+
+from collections import defaultdict
+
+# Keys are interests, values are list of user_ids with that interest
+user_ids_by_interest = defaultdict(list)
+
+for user_id, interest in interests:
+  user_ids_by_interest[interest].append(user_id)
+  
+# Keys are user_ids, values are lists of interests for that user_id.
+interests_by_user_id = defaultdict(list)
+
+for user_id, interest in interests:
+  interests_by_user_id[user_id].append(interest)
+  
+print("user_ids_by_interest", user_ids_by_interest)
+print("interests_by_user_id", interests_by_user_id)
+
+def most_common_interests_with(user):
+    return Counter(
+        interested_user_id
+        for interest in interests_by_user_id[user["id"]]
+        for interested_user_id in user_ids_by_interest[interest]
+        if interested_user_id != user["id"]
+    )
+
+print('most common interests with user[1]', most_common_interests_with(users[1]))
